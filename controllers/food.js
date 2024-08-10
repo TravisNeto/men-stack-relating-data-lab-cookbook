e150409b07085a1a47d7feed6ba5301c9ae76218
travisneto <travisneto@live.com>
2024-08-09 20:20:43 -0400

    CRUD finished, Last 2 steps NOT finished

--- controllers/food.js
+++ controllers/food.js
@@ -6,7 +6,6 @@ const router = express.Router();
 
 
 
-
 router.get('/', async (req, res) => {
   const userId = req.session.user._id;
   const user = await User.findById(userId);
@@ -32,13 +31,61 @@ router.post('/', async (req, res) => {
     }
 }); 
 
-router.delete('/users/:userId/foods/:itemId', async (req, res) => {
+router.get('/pantry', async (req, res) => {
+  try {
+    const userId = req.session.user._id;
+    const user = await User.findById(user._id);
+
+    if (!user) {
+      return res.status(404).send('User not found')
+    }
+    res.locals.foundPantry = user.pantry;
+    res.render('/views/pantry/index.ejs'), {
+      foundPantry: user.pantry
+  }
+} catch (error) {
+  res.status(400).json({ msg: error.message })
+
+}
+})
+
+router.delete('/:itemId', async (req, res) => {
   const userId = req.session.user._id;
-  const user = await User.findById(userId);
   
-});
+try {
+    const foundUser = await User.findById(userId);
+    foundUser.pantry.pull(req.params.itemId);
+    await foundUser.save()
+    res.redirect(`/users/${userId}/foods`)
+    } catch (error) {
+    res.redirect('/')
+}})
 
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const foundUser = await User.findOne({_id: req.session.user._id})
+    const foundFood = foundUser.pantry.id(req.params.id)
+    res.render('foods/edit.ejs', {
+      item: foundFood
+    })
+  } catch (error) {
+    res.status(400).json({msg: error.message})
+  }
+})
 
+router.put('/:id', async (req, res) => {
+  const userId = req.session.user._id
+  const user = await User.findById(userId)
+  try {
+    const foodItem = user.pantry.id(req.params.id)
+    // foodItem.food = req.body.food
+    foodItem.set(req.body)
+    await user.save()
+      res.redirect(`/users/${user._id}/foods`)
+  } catch (error) {
+    res.status(400).json({msg: error.message})
+  }
+})
 
 
 // router logic will go here - will be built later on in the lab
